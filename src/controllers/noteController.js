85d021f9d0b5fb20d8cfb81882a838958f09eba2
labmember003 <93467641+labmember003@users.noreply.github.com>
2023-05-20 23:39:15 +0530

    deplyment left

--- src/controllers/noteController.js
+++ src/controllers/noteController.js
@@ -17,12 +17,36 @@ const createNote = async (req, res) => {
     }
 }
 
-const updateNote = (req, res) => {
-    console.log(req.usedId);
+const updateNote = async (req, res) => {
+    const id = req.params.id;
+    const {title, description} = req.body;
+    
+    const newNote = new noteModel({
+        title: title,
+        description: description,
+        userId: req.usedId,
+        id: id,
+        _id: id
+    });
+    try {
+        await noteModel.findByIdAndUpdate({_id: id}, newNote, {new: true});
+        // upsert
+        res.status(201).json(newNote);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message: "Something went wrong"});
+    }
 }
 
-const deleteNote = (req, res) => {
-    console.log(req.usedId);
+const deleteNote = async (req, res) => {
+    const id = req.params.id;
+    try {
+        const note = await noteModel.findByIdAndDelete(id); 
+        res.status(202).json(note);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message: "Something went wrong"});
+    }
 }
 
 const getNotes = async (req, res) => {
